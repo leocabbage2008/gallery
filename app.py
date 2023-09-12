963de0e9c8eca22cc0fb61a39d3ab8c8f6e8215a
Leo <leocabbage2008@gmail.com>
2023-09-11 22:05:46 -0400

    e

--- app.py
+++ app.py
@@ -1,38 +1,63 @@
-from flask import Flask, render_template, send_from_directory
-from glob import glob
+from flask import Flask, render_template, send_from_directory, request
+from markupsafe import escape
+from flask_caching import Cache, CachedResponse
 import os, shutil
 
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 try:
-    dest = "./saves"
-    for filename in glob("/Users/coder/saves/*.*"):
-        shutil.copy(filename, dest)
+    shutil.copytree("/Users/coder/saves", "./static/saves", dirs_exist_ok=True)
 finally:
-    saves = "./saves"
+    saves = "./static/saves"
+
+try:
+    shutil.copytree("/Users/coder/Downloads/cats", "./static/cats", dirs_exist_ok=True)
+finally:
+    cats = "./static/cats"
 
 
 @app.route("/")
 def home():
-    return "parked"
+    return render_template("index.html")
 
 
-@app.route("/images/<filename>")
-def serve_image(filename):
-    path = os.path.join(saves, filename)
-    if not os.path.exists(path):
-        return "Image not found."
-    return send_from_directory(saves, filename)
+@app.route("/gallery/<path:filename>")
+def serve(filename):
+    # path = os.path.join(request.args.get("path"), filename)
+    # print(request.args.get("path") + " " + path + " Not found.")
+    # if not os.path.exists(path):
+    #     return request.args.get("path") + " " + path + " Not found."
+    # return send_from_directory("static", path)
+    return send_from_directory(
+        "static", os.path.join(request.args.get("path"), filename)
+    )
 
 
-@app.route("/image_gallery")
-def image_gallery():
-    images = [
-        (f, f.split(".")[1])
-        for f in os.listdir(saves)
-        if os.path.isfile(os.path.join(saves, f))
-    ]
-    return render_template("image_gallery.html", images=images)
+@app.route("/gallery")
+@cache.cached(timeout=300)
+def gallery():
+    password = request.args.get("password")
+    print(password)
+    if password == "pjoqfjopehnklewjjop56348890":
+        images = [
+            (f, f.split(".")[1], "./saves")
+            for f in os.listdir(saves)
+            if os.path.isfile(os.path.join(saves, f))
+        ]
+    else:
+        images = [
+            (f, f.split(".")[1], "./cats")
+            for f in os.listdir(cats)
+            if os.path.isfile(os.path.join(cats, f))
+        ]
+    return render_template("gallery.html", images=images)
 
 
 @app.route("/read/<text>")
@@ -48,4 +73,4 @@ def static_files(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
