2f045bfd7b32c18a5ed68cc83bb1ee52add4f7e4
Leo <leocabbage2008@gmail.com>
2024-07-16 22:01:40 -0400

    code so messy :3

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
-from flask import (
-    Flask,
-    render_template,
-    send_from_directory,
-    request,
-    redirect,
-    url_for,
-)
-from markupsafe import escape
-from pathlib import Path
-import random as r
-import os, shutil
+from flask import Flask, render_template, request, redirect
+import random
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
 
 app = Flask(__name__)
 
-# cache user options
-# make right click copy optional
-# make left click download optional
-# figure out issue on azure where mp4's dont work well
-# make some type of searching algo
-# make a sorting algo
+s3 = boto3.client("s3")
+bucket = "cranky"
+saves = "saves/"
+cats = "cats/"
 
 
-# try:
-#     shutil.copytree("/Users/coder/saves", "./static/saves", dirs_exist_ok=True)
-# except:
-#     print("on virty pullin up with a hundre fitty")
-saves = "./static/saves"
-cats = "./static/cats"
-images = []
+def listFiles(bucket, prefix):
+    response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    files = [obj["Key"] for obj in response.get("Contents", []) if obj["Key"] != prefix]
+    return files
+
+
+def url(filename, f):
+    return f"https://{bucket}.s3.us-east-2.amazonaws.com/{f}{filename}"
+
+
+saveFiles = listFiles(bucket, saves)
+catFiles = listFiles(bucket, cats)
+allFiles = saveFiles + catFiles
+
+
+def filter(ext):
+    return [f for f in allFiles if f.split(".")[-1].lower() in ext]
+
+
+def getPrefix(image):
+    if image in listFiles(bucket, saves):
+        return saves
+    return cats
 
 
 @app.route("/")
@@ -35,58 +41,40 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/gallery/<path:filename>")
-def serve(filename):
-    return send_from_directory(
-        "static", os.path.join(request.args.get("path"), filename)
-    )
-
-
-@app.route("/random")
-def random():
-    images = [f for f in os.listdir(saves) if f.split(".")[-1] in "pnggifjpgjpegwebp"]
-    i = r.choice(images)
-    return send_from_directory("static", os.path.join("./saves", i))
-
-
-@app.route("/randow")
-def randow():
-    videos = [f for f in os.listdir(saves) if f.split(".")[-1] in "webmmp4"]
-    x = r.choice(videos)
-    return send_from_directory("static", os.path.join("./saves", x))
-
-
 @app.route("/gallery")
 def gallery():
-    password = request.args.get("password")
-    if password == "pjoqfjopehnklewjjop56348890":
-        images = [
-            (str(f).split(r"/")[-1], str(f).split(r"/")[-1].split(".")[1], "./saves")
-            for f in sorted(
-                [x for x in Path(saves).iterdir() if not x.is_dir()],
-                key=os.path.getmtime,
-            )
-        ]
-    else:
-        images = [
-            (f, f.split(".")[1], "./cats")
-            for f in os.listdir(cats)
-            if os.path.isfile(os.path.join(cats, f))
-        ]
-    images = [x for x in images if x[1] in "pnggifjpgjpegwebp"]
+    password = request.args.get("password") == "pjoqfjopehnklewjjop56348890"
+    images = [
+        (
+            f.split("/")[-1],
+            f.split(".")[-1],
+            url(f.split("/")[-1], saves if password else cats),
+        )
+        for f in sorted(saveFiles if password else catFiles)
+    ]
+    images = [
+        x for x in images if x[1].lower() in ["png", "gif", "jpg", "jpeg", "webp"]
+    ]
     return render_template("gallery.html", images=images)
 
 
-@app.route("/read/<text>")
-def nanny(text):
-    return f"""lemme read you a bed time story kid
-  once upon a time,
-  {escape(text)}"""
+@app.route("/serve/<path:filename>")
+def serve(filename):
+    return redirect(url(filename, getPrefix(filename)))
 
 
-@app.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory("static", filename)
+@app.route("/random")
+def random_image():
+    images = filter(["png", "gif", "jpg", "jpeg", "webp"])
+    r = random.choice(images)
+    return redirect(url(r), getPrefix(r))
+
+
+@app.route("/randow")
+def random_video():
+    videos = filter(["webm", "mp4"])
+    r = random.choice(videos)
+    redirect(url(r), getPrefix(r))
 
 
 if __name__ == "__main__":
